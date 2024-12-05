41279e88fe766338df7c0a0ac7ec13181352a135
kws <kws1207@kaist.ac.kr>
2024-12-06 03:00:27 +0900

    Self apply reviews

--- sui_client/src/App.tsx
+++ sui_client/src/App.tsx
@@ -1,11 +1,7 @@
-import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import Challenge_1 from "./Pages/Challenge_1";
 import Challenge_2 from "./Pages/Challenge_2";
-// import Challenge_3 from "./Pages/Challenge_3";
-// import Challenge_4 from "./Pages/Challenge_4";
-import { ConnectButton } from "@mysten/dapp-kit"; // DAppProvider 추가
 
 function App() {
   return (
@@ -14,12 +10,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/challenge-1" element={<Challenge_1 />} />
         <Route path="/challenge-2" element={<Challenge_2 />} />
-        {/* <Route path="/challenge-3" element={<Challenge_3 />} />
-        <Route path="/challenge-4" element={<Challenge_4 />} /> */}
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
