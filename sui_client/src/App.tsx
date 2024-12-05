79869e3e97a134950afaf0962cefdaaf91f6957c
97woo <kicaptain16@gmail.com>
2024-12-06 02:21:10 +0900

    fix App.tsx

--- sui_client/src/App.tsx
+++ sui_client/src/App.tsx
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import Challenge_1 from "./Pages/Challenge_1";
 import Challenge_2 from "./Pages/Challenge_2";
-import Challenge_3 from "./Pages/Challenge_3";
-import Challenge_4 from "./Pages/Challenge_4";
+// import Challenge_3 from "./Pages/Challenge_3";
+// import Challenge_4 from "./Pages/Challenge_4";
 import { ConnectButton } from "@mysten/dapp-kit"; // DAppProvider 추가
 
 function App() {
@@ -14,8 +14,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/challenge-1" element={<Challenge_1 />} />
         <Route path="/challenge-2" element={<Challenge_2 />} />
-        <Route path="/challenge-3" element={<Challenge_3 />} />
-        <Route path="/challenge-4" element={<Challenge_4 />} />
+        {/* <Route path="/challenge-3" element={<Challenge_3 />} />
+        <Route path="/challenge-4" element={<Challenge_4 />} /> */}
       </Routes>
     </Router>
   );
