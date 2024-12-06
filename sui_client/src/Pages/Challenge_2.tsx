9ec8d9228359505c475ac115c6d520940f864bdb
97woo <kicaptain16@gmail.com>
2024-12-06 16:12:23 +0900

    fix Challeng2 <Pre> indent

--- sui_client/src/Pages/Challenge_2.tsx
+++ sui_client/src/Pages/Challenge_2.tsx
@@ -210,7 +210,8 @@ module 0x0::flash{
         if (vec_map::contains(&self.lender, &sender)) {
             let balance = vec_map::get_mut(&mut self.lender, &sender);
             *balance = *balance + coin::value(&coin);
-        } else {
+        } 
+        else {
             vec_map::insert(&mut self.lender, sender, coin::value(&coin));
         };
         coin::put(&mut self.to_lend, coin);
@@ -238,6 +239,7 @@ module 0x0::flash{
         }
     }
 }
+
   `}
       </pre>
       <div
