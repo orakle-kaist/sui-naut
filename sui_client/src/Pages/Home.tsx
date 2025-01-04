b916f4f84ffa9c2e1458f2ff15c6d570ca443895
97woo <kicaptain16@gmail.com>
2025-01-04 18:15:18 +0900

    Refactor: ChallengeGrid component and complete Home page layout

--- sui_client/src/Pages/Home.tsx
+++ sui_client/src/Pages/Home.tsx
@@ -1,9 +1,11 @@
 //import { useNavigate } from "react-router-dom";
-import Header  from "../components/layout/Header";
-import Footer  from "../components/layout/Footer";
+import Header        from "../components/layout/Header";
+import Footer        from "../components/layout/Footer";
+import ChallengeGrid from "../components/ChallengeGrid";
 
-import arrowIcon  from "../assets/images/arrow.png"
+import arrowIcon       from "../assets/images/arrow.png"
 import astronautFloat  from "../assets/images/astronautFloat.png"
+import astronautDesk   from "../assets/images/astronautDesk.png"
 
 function Home() {
   //const navigate = useNavigate();
@@ -19,12 +21,17 @@ function Home() {
           <br />
           Test your skills in Sui Move with fun and engaging challenges.
         </p>
-        <div className="absolute -right-0 top-75">
+        <div className="absolute right-0 top-75 2xl:right-0 2xl:top-50">
           <img src={astronautFloat} alt="Floating Astronaut" className="w-[380px] h-[350px]  2xl:w-[450px] 2xl:h-[450px]"/>
         </div>
         <img src={arrowIcon} alt="arrow" className="w-8 h-5" />
+        <div className="flex items-center justify-center mt-10">
+        <ChallengeGrid />
+        </div>
+        <div className="absolute -left-12 -bottom-10">
+          <img src={astronautDesk} alt="Floating Astronaut" className="w-[380px] h-[350px]  2xl:w-[450px] 2xl:h-[450px]"/>
+        </div>
       </div>
-
       <Footer />
     </div>
   );
