ddbfc3d6603f75f04e22630b7fa5b9bb7fb380ca
97woo <kicaptain16@gmail.com>
2025-01-04 15:31:15 +0900

    fix: adjust footer image position

--- sui_client/src/components/layout/Footer.tsx
+++ sui_client/src/components/layout/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => {
     return (
         <footer className="container mx-auto px-4 py-2 text-center text-white  font-semibold relative font-sans">
             <p>© 2024 Suinaut | Powered by KAIST Blockchain Research Society ORAKLE</p>
-            <div className="fixed bottom-0 right-0 m-3">
+            <div className="fixed bottom-1 right-1">
                 <img src={suiLogo} alt="sui_logo" className="w-8 h-8" />
             </div>
         </footer>
