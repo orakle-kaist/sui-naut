f780a74dbb2c0a68150c8030435e04774018bc9a
97woo <kicaptain16@gmail.com>
2025-01-06 01:09:22 +0900

    fix: adjust indentation in footer.tsx and ChallgeGrid.tsx

--- sui_client/src/components/layout/Footer.tsx
+++ sui_client/src/components/layout/Footer.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import suiLogo from "../../assets/images/sui_logo.png";
 
 const Footer: React.FC = () => {
-    return (
-        <footer className="container mx-auto px-4 py-2 text-center text-white  font-semibold relative font-sans">
-            <p>© 2024 Suinaut | Powered by KAIST Blockchain Research Society ORAKLE</p>
-            <div className="fixed bottom-1 right-1">
-                <img src={suiLogo} alt="sui_logo" className="w-8 h-8" />
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="container mx-auto px-4 py-2 text-center text-white  font-semibold relative font-sans">
+      <p>© 2024 Suinaut | Powered by KAIST Blockchain Research Society ORAKLE</p>
+      <div className="fixed bottom-1 right-1">
+        <img src={suiLogo} alt="sui_logo" className="w-8 h-8" />
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
