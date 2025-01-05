56b78ba9ca7b41a36afc0335b4512551347e52d4
97woo <kicaptain16@gmail.com>
2025-01-06 02:08:27 +0900

    Fix Footer padding

--- sui_client/src/components/layout/Footer.tsx
+++ sui_client/src/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import suiLogo from "../../assets/images/sui_logo.png";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="w-full text-white py-4 relative">
+    <footer className="w-full text-white py-1 relative">
       <p className="text-center font-semibold">© 2024 Suinaut | Powered by KAIST Blockchain Research Society ORAKLE</p>
       <div className="absolute bottom-0 right-0 m-1">
         <img src={suiLogo} alt="sui_logo" className="w-8 h-8" />
